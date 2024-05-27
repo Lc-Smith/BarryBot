31abd41a0f3d664ed6070b2fd98f2f13d1a84052
Dexvo <63011374+Dexvoo@users.noreply.github.com>
2024-05-27 20:09:41 +0100

    Introducing mongoDB database

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,20 @@
 const { Events } = require('discord.js');
+const mongoose = require('mongoose');
+const { dbUrl, dbName } = require('../config.json');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
+
+		const dbString = `${dbUrl}/${dbName}`;
+		mongoose.set('strictQuery', false);
+		await mongoose.connect(dbString).then(() => {
+			console.log('Connected to database!');
+		}).catch((err) => {
+			console.log('Error connecting to database: ' + err);
+		});
+
 		console.log("Ready!");
 	    client.user.setActivity("with Lc's feelings");
 	},
